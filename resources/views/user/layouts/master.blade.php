ef21262d68c72968f9393bd1ecb75172966b191e
Pat <pat.hornchaiya@hotmail.com>
2015-12-26 16:43:11 +0700

    Add Materialize and more

--- resources/views/user/layouts/master.blade.php
+++ resources/views/user/layouts/master.blade.php
@@ -2,93 +2,43 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>@yield('title')</title>
+    <!-- <link href="css/pe-icon-7-stroke.css" rel="stylesheet" /> -->
+    <!-- <link href="css/ct-navbar.css" rel="stylesheet" /> -->
     <link href="css/bootstrap.css" rel="stylesheet" />
-    <link href="css/pe-icon-7-stroke.css" rel="stylesheet" />
-    <link href="css/ct-navbar.css" rel="stylesheet" />
+    <link href="css/materialize.min.css" rel="stylesheet" />
     <link href="css/app.css" rel="stylesheet" />
-
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <script src="js/jquery-1.10.2.js"></script>
+    <!-- <script src="js/ct-navbar.js"></script> -->
     <script src="js/bootstrap.js"></script>
-    <script src="js/ct-navbar.js"></script>
+    <script src="js/materialize.min.js"></script>
   </head>
-  <body>
+  <body class="main-body">
     @section('navbar')
-    <div id="navbar-full">
-       <div id="navbar">
-          <!--
-           navbar-default can be changed with navbar-ct-blue navbar-ct-azzure navbar-ct-red navbar-ct-green navbar-ct-orange
-           -->
-           <nav class="navbar navbar-ct-blue navbar-fixed-top" role="navigation">
-
-             <div class="container">
-               <!-- Brand and toggle get grouped for better mobile display -->
-               <div class="navbar-header">
-                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                   <span class="sr-only">Toggle navigation</span>
-                   <span class="icon-bar"></span>
-                   <span class="icon-bar"></span>
-                   <span class="icon-bar"></span>
-                 </button>
-                 <a class="navbar-brand navbar-brand-logo" href="http://www.creative-tim.com">
-                       <div class="logo">
-                       <img id="avatar" src="{{Auth::user()->avatar}}">
-                       </div>
-                       <div class="brand"> <span>จดหมาย</span> <br> ของ {{Auth::user()->name}} </div>
-                 </a>
-               </div>
-               <!-- Collect the nav links, forms, and other content for toggling -->
-               <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                 <ul class="nav navbar-nav navbar-right">
-                       <!-- <li>
-                           <a href="javascript:void(0);" data-toggle="search" class="hidden-xs">
-                               <i class="pe-7s-search"></i>
-                               <p>Search</p>
-                           </a>
-                       </li> -->
-                       <li>
-                           <a href="#">
-                               <i class="pe-7s-mail">
-                                   <span class="label">+</span>
-                               </i>
-                               <p>สร้างจดหมายใหม่</p>
-                           </a>
-                       </li>
-                       <li class="dropdown">
-                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                   <i class="pe-7s-user"></i>
-                                   <p>บัญชีผู้ใช้</p>
-                               </a>
-                             <ul class="dropdown-menu">
-                               <li><a href="#">Action</a></li>
-                               <li><a href="#">Another action</a></li>
-                               <li><a href="#">Something</a></li>
-                               <li><a href="#">Another action</a></li>
-                               <li><a href="#">Something</a></li>
-                               <li class="divider"></li>
-                               <li><a href="#">Separated link</a></li>
-                             </ul>
-                       </li>
-                  </ul>
-                  <form class="navbar-form navbar-right navbar-search-form" role="search">
-                    <div class="form-group">
-                         <input type="text" value="" class="form-control" placeholder="Search...">
-                    </div>
-                 </form>
-
-               </div><!-- /.navbar-collapse -->
-             </div><!-- /.container-fluid -->
-           </nav>
-          <!-- <div class="blurred-container">
-               <div class="img-src" style="background-image: url('images/bg.jpg')"></div>
-           </div> -->
-       </div><!--  end navbar -->
-
-   </div> <!-- end menu-dropdown -->
+      <ul id="dropdown1" class="dropdown-content">
+        <li><a href="#!"><img src="{{Auth::user()->avatar}}" alt="" class="circle responsive-img avatar-icon"> {{Auth::user()->name}}</a></li>
+        <li class="divider"></li>
+        <!-- <li><a href="#!">two</a></li> -->
+        <!-- <li class="divider"></li> -->
+        <li><a href="#!">ออกจากระบบ</a></li>
+      </ul>
+      <nav class="blue lighten-2">
+        <div class="nav-wrapper container">
+          <a href="#!" class="brand-logo">จดหมาย</a>
+          <ul class="right hide-on-med-and-down">
+            <li><a href="sass.html">จดหมายใหม่</a></li>
+            <!-- <li><a href="badges.html">Components</a></li> -->
+            <!-- Dropdown Trigger -->
+            <li><a class="dropdown-button" href="#!" data-activates="dropdown1">คุณ {{Auth::user()->name}}<i class="material-icons right">arrow_drop_down</i></a></li>
+          </ul>
+        </div>
+      </nav>
 
     @show
 
-    <div class="container">
+    <div class="container main-container">
       @yield('content')
     </div>
 
--- resources/views/user/mainpage.blade.php
+++ resources/views/user/mainpage.blade.php
@@ -3,7 +3,60 @@
 @section('title', 'ยินดีต้อนรับสู่จดหมาย')
 
 @section('content')
-  <p>
-    This is main page
-  </p>
+  <!-- <div class="col-md-4">
+    <div class="card-panel collection">
+      <div class="collection-item center-align">
+        <img src="{{Auth::user()->avatar}}" class="circle responsive-img z-depth-1" alt="" />
+        <br>
+        คุณ {{Auth::user()->name}}
+      </div>
+      <div class="collection-item">
+        <img src="{{Auth::user()->avatar}}" class="circle responsive-img center-align z-depth-1" alt="" />
+      </div>
+    </div>
+  </div> -->
+  <div class="col-md-4">
+    <div class="row main-user-info">
+      <div class="col-md-4">
+        <img src="{{Auth::user()->avatar}}" class="circle responsive-img z-depth-1" alt="" />
+      </div>
+      <div class="col-md-8 left-align nopad valign-wrapper">
+        <p class="valign">
+          <span class="username">คุณ {{Auth::user()->name}}</span>
+          <br>
+          คุณมีเครดิต {{Auth::user()->credits}} points
+        </p>
+      </div>
+    </div>
+
+    <div class="row side-menu">
+        <a href="#anounce"><i class="glyphicon glyphicon glyphicon-user"></i>  <span> จัดการบัญชี</span></a>
+    </div>
+
+    <div class="row side-menu">
+        <a href="#anounce"><i class="glyphicon glyphicon-log-out"></i>  <span> ออกจากระบบ</span></a>
+    </div>
+
+    <hr>
+
+    <div class="row side-menu">
+        <a href="#main-manage"><i class="glyphicon glyphicon-th-list"></i>  <span> การจัดการหลัก</span></a>
+    </div>
+
+    <div class="row side-menu">
+        <a href="#anounce"><i class="glyphicon glyphicon-bullhorn"></i>  <span> ประชาสัมพันธ์</span></a>
+    </div>
+
+    <div class="row side-menu">
+        <a href="#anounce"><i class="glyphicon glyphicon glyphicon-bell"></i>  <span> แจ้งเตือนของคุณ</span></a>
+    </div>
+
+
+  </div>
+
+  <div class="col-md-8">
+    <div class="card-panel">
+
+    </div>
+  </div>
 @endsection
