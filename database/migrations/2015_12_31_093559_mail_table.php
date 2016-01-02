2970516897b657f8b6cdac04d97da47314785085
Pat <pat.hornchaiya@hotmail.com>
2016-01-02 16:45:07 +0700

    Add migration for mail-related tables

---
+++ database/migrations/2015_12_31_093559_mail_table.php
@@ -0,0 +1,74 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MailTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Create Mail categories table
+        Schema::create('categories', function(Blueprint $table) {
+          $table->increments('id');
+          $table->string('name');
+          $table->string('description');
+          $table->string('picture');
+        });
+
+        // Create Mail-Type Table
+        Schema::create('mailtypes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description');
+            $table->string('picture_url');
+            $table->string('price');
+            $table->integer('category_id')->unsigned();
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+
+        // Create Address Table
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('name');
+            $table->string('address_line_1');
+            $table->string('address_line_2');
+            $table->string('address_line_3');
+            $table->string('address_line_4');
+            $table->string('postcode');
+        });
+
+        // Create Mail Table
+        Schema::create('mails', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('mail_type_id')->unsigned();
+            $table->foreign('mail_type_id')->references('id')->on('mailtypes');
+            $table->string('mail_url');
+            $table->string('other');
+            $table->integer('address_id')->unsigned();
+            $table->foreign('address_id')->references('id')->on('addresses');
+            $table->integer('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('categories');
+        Schema::drop('mailtypes');
+        Schema::drop('addresses');
+        Schema::drop('mails');
+    }
+}
---
+++ resources/views/mail/createMail.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.master')
+
+@section('title', 'สร้างจดหมายใหม่')
+
+@section('content')
+  <!-- <div class="col-md-4">
+    <div class="card-panel collection">
+      <div class="collection-item center-align">
+        <img src="{{Auth::user()->avatar}}" class="circle responsive-img z-depth-1" alt="" />
+        <br>
+        คุณ {{Auth::user()->name}}
+      </div>
+      <div class="collection-item">
+        <img src="{{Auth::user()->avatar}}" class="circle responsive-img center-align z-depth-1" alt="" />
+      </div>
+    </div>
+  </div> -->
+
+  <div class="col-md-12">
+
+    <h5>สร้างจดหมายใหม่</h5>
+
+    <div class="card medium row hoverable">
+      <div class="card-image">
+        <img src="{{url('images/slider/1.jpg')}}">
+        <span class="card-title">ประชาสัมพันธ์</span>
+      </div>
+      <div class="card-content">
+        <p>I am a very simple card. I am good at containing small bits of information.
+        I am convenient because I require little markup to use effectively.</p>
+      </div>
+      <div class="card-action ">
+        <a href="#">ดูรายละเอียด</a>
+      </div>
+    </div>
+
+    <!-- =================== Notification Cards =================== -->
+    <h5>แจ้งเตือนของคุณ</h5>
+
+    <div class="card row hoverable">
+      <div class="card-content">
+        <div class="row nomargin">
+          <div class="col-md-2 center-align">
+            <i class="glyphicon glyphicon-send card-icon"></i>
+          </div>
+          <div class="col-md-10">
+            <p class="card-topic">กำลังจัดส่ง</p>
+            I am a very simple card. I am good at containing small bits of information.
+            I am convenient because I require little markup to use effectively.
+          </div>
+        </div>
+
+      </div>
+      <div class="card-action ">
+        <a href="#">ดูรายละเอียด</a>
+      </div>
+    </div>
+
+  </div>
+@endsection
