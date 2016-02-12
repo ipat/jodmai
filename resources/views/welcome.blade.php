61f6c7026e8a01f0cba50b6de41bb174c6494c69
nobpo <thelast_chapter@hotmail.com>
2016-02-12 18:10:34 +0700

    แก้ราคา

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,7 +71,7 @@
             <span class="icon-bar"></span>
           </button>
           <a class="navbar-brand" href="#">
-            <h1 class="animated fadeInLeftBig"><span style="color:#fff">www.jod-mai.com</span></h1>
+            <h1 class="animated fadeInLeftBig"><span style="color:#fff">jod-mai.com</span></h1>
             <!-- <h1><img class="img-responsive" src="{{url('images/logo3.png')}}" alt="logo"></h1> -->
           </a>
         </div>
@@ -127,7 +127,7 @@
           <div class="col-md-4 col-md-offset-4 text-center">
             <a href="auth/facebook"><p class="facebook-button">เข้าสู่ระบบผ่าน Facebook</p></a>
           </div>
-          
+
           <div class="col-md-4 col-md-offset-4 text-center">
             <a href="auth/facebook"><p class="facebook-button">สมัครผ่าน Facebook</p></a>
           </div>
@@ -454,10 +454,10 @@
             <div class="single-table wow flipInY" data-wow-duration="1000ms" data-wow-delay="500ms">
               <h3>ผ่านบัตรเครดิต</h3>
               <div class="price">
-                20 <span>บาท</span>
+                50 <span>บาท</span>
               </div>
               <ul>
-                <li>ได้ 20 point</li>
+                <li>ได้ 40 point</li>
                
               </ul>
               <a href="#login" class="btn btn-lg btn-primary">Sign up</a>
@@ -480,11 +480,11 @@
             <div class="single-table wow flipInY" data-wow-duration="1000ms" data-wow-delay="1100ms">
               <h3>ผ่านระบบ paypal</h3>
               <div class="price">
-                20<span>บาท</span>
+                50<span>บาท</span>
               </div>
               <ul>
                 
-                <li>ได้ 20 point</li>
+                <li>ได้ 40 point</li>
                 <!-- <li>500 character</li> -->
               </ul>
               <a href="#login" class="btn btn-lg btn-primary">Sign up</a>
